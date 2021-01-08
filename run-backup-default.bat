967d1e0a5be1d82d5dde2c0c98095bd1c0c971f2
Dor-B <dor46200@gmail.com>
2021-01-08 17:26:12 +0200

    Add simple backup windows script

---
+++ run-backup-default.bat
@@ -0,0 +1,6 @@
+ECHO OFF
+ECHO (Make sure first time to install Node.js and run npm install in this folder)
+ECHO BACKING UP EVRYTHING INTO firebase-backup folder
+node firebase-backup.js
+ECHO DONE
+PAUSE
