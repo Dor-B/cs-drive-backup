c6e07ff8aefcc31c16ab7057f3d58c51237d3366
Dor-B <dor46200@gmail.com>
2021-01-18 01:03:21 +0200

    Backup default

--- run-backup-default.bat
+++ run-backup-default.bat
@@ -1,6 +1,8 @@
 ECHO OFF
-ECHO (Make sure first time to install Node.js and run npm install in this folder)
+ECHO (Make sure first time to clone using git, install Node.js and run npm install in this folder)
 ECHO BACKING UP EVRYTHING INTO firebase-backup folder
 node firebase-backup.js
-ECHO DONE
+ECHO UPLOADING TO GITHUB
+git add -A
+git commit -m "Backup default"
 PAUSE
