3b9954d323b46b6476d0ea1fd9b768bf16bb7968
Dor-B <dor46200@gmail.com>
2021-01-18 12:06:28 +0200

    Backup default

--- run-backup-default.bat
+++ run-backup-default.bat
@@ -1,5 +1,4 @@
 ECHO OFF
-cd "C:\Users\Dor\Desktop\cs-drive\cs-drive-backup"
 ECHO (Make sure first time to clone using git, install Node.js and run npm install in this folder)
 ECHO BACKING UP EVRYTHING INTO firebase-backup folder
 node firebase-backup.js
